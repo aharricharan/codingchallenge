8e24934bd873ae318cd1162b7f917852a8c9c8ad
aharricharan <30323410+aharricharan@users.noreply.github.com>
2017-10-15 13:30:10 -0400

    Update photosAction.js

--- src/actions/photosAction.js
+++ src/actions/photosAction.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export function fetchPhotos(galleryId) {
    return function(dispatch) {
-   	axios.get("https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=2c328fd915033ec17eb1d56cb43585b0&gallery_id=" + galleryId + "&extras=owner_name%2Cdate_taken%2Cviews%2Curl_t%2Curl_l&format=json&nojsoncallback=1")
+   	axios.get("https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=e37844736a9fab6f55a9b581f0e92009&gallery_id=" + galleryId + "&extras=owner_name%2Cdate_taken%2Cviews%2Curl_t%2Curl_l&format=json&nojsoncallback=1")
    	.then((response) => {
    		dispatch({
    			type:"FETCH_PHOTOS",
